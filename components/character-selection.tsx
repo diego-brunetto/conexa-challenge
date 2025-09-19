475edb6681fb42274ec4bbee08fa49f979ee9912
diego brunetto <diego.brunetto2@gmail.com>
2025-09-19 11:47:03 -0300

    feat: add character selection with data instanced for each

---
+++ components/character-selection.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useCharacterContext } from "@/contexts/character-context";
+import { useCharacterSelection } from "@/hooks/use-character-selection";
+import type { ApiResponse, Character } from "@/lib/types";
+import { CharacterList } from "./character-list";
+
+interface CharacterSelectionProps {
+	initial: {
+		characters: Character[];
+		info: ApiResponse<Character>["info"] | null;
+	};
+}
+
+export function CharacterSelection({ initial }: CharacterSelectionProps) {
+	const { selectedCharacters, handleCharacterSelect } = useCharacterContext();
+
+	const cs1 = useCharacterSelection(initial);
+	const cs2 = useCharacterSelection(initial);
+
+	return (
+		<div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+			<CharacterList
+				title="Character #1"
+				characters={cs1.characters}
+				selectedCharacter={selectedCharacters.character1}
+				onCharacterSelect={(character) => handleCharacterSelect(character, 1)}
+				loading={cs1.loading}
+				currentPage={cs1.page}
+				totalPages={cs1.info?.pages || 1}
+				onPageChange={cs1.setPage}
+			/>
+
+			<CharacterList
+				title="Character #2"
+				characters={cs2.characters}
+				selectedCharacter={selectedCharacters.character2}
+				onCharacterSelect={(character) => handleCharacterSelect(character, 2)}
+				loading={cs2.loading}
+				currentPage={cs2.page}
+				totalPages={cs2.info?.pages || 1}
+				onPageChange={cs2.setPage}
+			/>
+		</div>
+	);
+}
